cc039f0e02d7fb4720953af9f5b066b006d1c6d9
tmorgan1987 <tmorgan1987@outlook.com>
2022-11-04 00:44:57 -0400

    got some stuff working, trying to get it to view the dang notes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,7 +1,5 @@
 const router = require('express').Router();
-const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
-const path = require('path');
 
 router.get('/notes', (req, res) => {
 	fs.readFile('./db/db.json', 'utf8', (err, data) => {
@@ -14,28 +12,17 @@ router.get('/notes', (req, res) => {
 );
 
 router.post('/notes', (req, res) => {
-	const { title, text } = req.body;
-	if (title && text) {
-		const newNote = {
-			title,
-			text,
-			note_id: uuidv4
-		};
-		readAppend(newNote, './db/db.json');
+	let notes = fs.readFileSync('./db/db.json');
+	notes = JSON.parse(notes);
+	res.json(notes);
+	let Note = {
+		title: req.body.title,
+		text: req.body.text,
+	};
+	notes.push(Note);
+	fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+	res.json(notes);
 
-		const response = {
-			status: 'success',
-			body: newNote,
-		};
-
-		res.json(response);
-	} else {
-		res.json('Error in posting feedback');
-	}
 });
 
 module.exports = router;
-
-// GET /api/notes should read the db.json file and return all saved notes as JSON.
-
-// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -3,12 +3,10 @@ const path = require('path');
 
 router.get('/notes', (req, res) => {
 	res.sendFile(path.join(__dirname, "../public/notes.html"))
-}
-);
+});
 
 router.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, "../public/index.html"))
-}
-);
+});
 
 module.exports = router;
