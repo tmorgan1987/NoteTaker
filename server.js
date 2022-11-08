4f55f892e94c3d4dc27332f0c61574680b0da466
tmorgan1987 <tmorgan1987@outlook.com>
2022-11-07 20:02:46 -0500

    capital port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const api = require('./routes/apiRoutes');
 const html = require('./routes/htmlRoutes');
 
 const app = express();
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
